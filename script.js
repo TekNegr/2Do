2bdb513132e829f22f5518bfd4036af86f703683
Henintsoa RAMAKAVELO <h.social74@gmail.com>
2023-04-25 11:13:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnADD = document.querySelector('#add')
+let input = document.querySelector('#new')
+const container = document.querySelector('#todo')
+
+btnADD.addEventListener('click',()=>{
+
+})
